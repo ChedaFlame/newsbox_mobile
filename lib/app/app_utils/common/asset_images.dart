37f4d28c5c7a7cdd79b1e095cb9f5b45ab35e55a
ChedaFlame <jarrelfrancis@gmail.com>
2025-10-15 15:15:08 +0200

    Delete lib directory

--- lib/app/app_utils/common/asset_images.dart
+++
@@ -1,9 +0,0 @@
-class LocalImages {
-
-  static const String logo = "assets/images/newsbox.png";
-  static const String avatar = "assets/images/avatar.png";
-  static const String placeholder = "assets/images/placeholder.png";
-
-
-
-}
